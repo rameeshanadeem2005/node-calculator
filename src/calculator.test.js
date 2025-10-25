2d1b056ff4cdee588714faacae47799e64684e0c
abcd <abcd@gmail.com>
2025-10-25 14:39:08 +0500

    Fix modulus test import

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,4 +1,4 @@
-const { add, subtract, multiply, divide } = require('./calculator');
+const calculator = require('./calculator');
 
 test('adds 2 + 3 to equal 5', () => {
   expect(add(2, 3)).toBe(5);
